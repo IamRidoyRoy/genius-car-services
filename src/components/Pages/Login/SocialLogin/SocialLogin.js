0eead9684436c25e43249bf8da7a0355b11a51cc
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-04-29 15:18:24 +0600

    Google sign in added

--- src/components/Pages/Login/SocialLogin/SocialLogin.js
+++ src/components/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,8 +2,27 @@ import React from 'react';
 import Google from '../../../../images/social/Google.png';
 import Facebook from '../../../../images/social/Facebook.png';
 import Github from '../../../../images/social/GitHub.png';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../Firebase.init'
+import { useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    let errorElement;
+    const navigate = useNavigate();
+    if (error) {
+        errorElement = (
+            <div>
+                <p className='text-danger text-center'><b>Error:</b> {error.message}</p>
+            </div>
+        );
+    }
+    if (loading) {
+        return <p className='text-center'>Loading...</p>;
+    }
+    if (user) {
+        navigate('/home');
+    }
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -11,7 +30,9 @@ const SocialLogin = () => {
                 <p className='mt-3 px-2'>OR</p>
                 <div style={{ height: '1px' }} className='bg-primary w-50'>  </div>
             </div>
-            <button className=' btn btn-info w-50 d-block mx-auto my-2'>
+            {errorElement}
+            <button onClick={() => signInWithGoogle()}
+                className=' btn btn-info w-50 d-block mx-auto my-2'>
                 <img height={30} src={Google} alt="" />
                 <span className='px-2'>Google SignIn</span>
             </button>
